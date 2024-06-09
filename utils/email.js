c196ebd805b482d741406ed925ed75f9d11beca9
Ondřej Polák <polak.ondrej95@gmail.com>
2024-06-10 00:30:19 +0200

    email

--- utils/email.js
+++ utils/email.js
@@ -1,15 +1,41 @@
 const nodemailer = require('nodemailer');
 
+// new Email (user, url).sendWelcome()
+
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `CloudyLake Team <${process.env.EMAIL_FROM}>`;
+  }
+
+  createTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid
+      return 1;
+    }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  send(template, subject) {
+    // Sent the actual email
+  }
+
+  sendWelcome() {
+    this.send('Welcome', 'Welcome to the CloudyLake Family!');
+  }
+};
+
 const sendEmail = (options) => {
-  // Transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
   // Define the email options
   const mailOptions = {
     from: 'Ondřej Polák <ondrej.polak@webgroo.com>',
@@ -23,5 +49,3 @@ const sendEmail = (options) => {
 
   transporter.sendMail(mailOptions);
 };
-
-module.exports = sendEmail;
