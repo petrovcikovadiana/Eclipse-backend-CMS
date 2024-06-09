665608e759d6c5fcada089cd5587204c8530cb38
Ondřej Polák <polak.ondrej95@gmail.com>
2024-06-09 20:26:14 +0200

    adding jwt token check EP

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -54,6 +55,9 @@ app.use(
     ],
   }),
 );
+
+// Cookie parser
+app.use(cookieParser());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
