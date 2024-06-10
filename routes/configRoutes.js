d537febb1cf95d55f86c9fc4ceccc9fd37a7891a
Ondřej Polák <polak.ondrej95@gmail.com>
2024-06-10 20:16:06 +0200

    protected EP

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const configController = require('../controllers/configController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
 router
   .route('/')
   .get(configController.getAllConfigs)
-  .post(configController.createConfig)
-  .patch(configController.updateConfig);
+  .post(authController.protect, configController.createConfig)
+  .patch(authController.protect, configController.updateConfig);
 
 router
   .route('/:config_key')
   .get(configController.getConfig)
-  .patch(configController.updateConfig)
-  .delete(configController.deleteConfig);
+  .patch(authController.protect, configController.updateConfig)
+  .delete(authController.protect, configController.deleteConfig);
 
 module.exports = router;
